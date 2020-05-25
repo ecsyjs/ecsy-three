8081b18bd665c5c38c4952504405fdf7cc92cf7d
netpro2k <netpro2k@gmail.com>
2020-05-24 18:52:27 -0700

    Add some tag components, change usage of camera

--- src/components/index.js
+++ src/components/index.js
@@ -34,3 +34,4 @@ export { Transform } from "./Transform.js";
 export { Visible } from "./Visible.js";
 export { VRController, VRControllerBasicBehaviour } from "./VRController.js";
 export { WebGLRenderer } from "./WebGLRenderer.js";
+export * from "./Object3DTags.js";
--- src/entity-utils.js
+++ src/entity-utils.js
@@ -1,10 +1,28 @@
-import { Object3D } from "./components/index.js";
+import {
+  Object3D,
+  MeshTagComponent,
+  SceneTagComponent,
+  CameraTagComponent
+} from "./components/index.js";
+
+function tagClassForObject3D(obj) {
+  // TODO support more tags and probably a way to add user defined ones
+  if (obj.isMesh) {
+    return MeshTagComponent;
+  } else if (obj.isScene) {
+    return SceneTagComponent;
+  } else if (obj.isCamera) {
+    return CameraTagComponent;
+  }
+}
 
 export function addObject3DComponents(entity, obj, parentEntity) {
   obj.entity = entity;
   entity.addComponent(Object3D, { value: obj });
-  // TODO add actual tag components based on type
-  // entity.addComponent(MeshTagComponent);
+  const Tag = tagClassForObject3D(obj);
+  if (Tag) {
+    entity.addComponent(Tag);
+  }
   if (parentEntity) {
     parentEntity.getComponent(Object3D).value.add(obj);
   }
@@ -16,7 +34,9 @@ export function removeObject3DComponents(entity, unparent = true) {
     obj.parent && obj.parent.remove(obj);
   }
   entity.removeComponent(Object3D);
-  // TODO remove actual tag components based on type, or maybe just all of the ecsy-three ones
-  // entity.removeComponent(MeshTagComponent);
+  const Tag = tagClassForObject3D(obj);
+  if (Tag) {
+    entity.removeComponent(Tag);
+  }
   obj.entity = null;
 }
