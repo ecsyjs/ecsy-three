2d8ea2376b89e9692f2ba774d6a556d442f07cd6
Fernando Serrano <fernandojsg@gmail.com>
2020-06-24 00:03:38 +0200

    core / extras

---
+++ src/core/index.js
@@ -0,0 +1,6 @@
+// CORE
+// export { initialize as initializeCore } from "./core/initialize.js";
+export { ECSYThreeWorld } from "./world.js";
+export * from "./Types";
+export * from "./components.js";
+export { defaultObject3DInflator } from "./defaultObject3DInflator.js";
