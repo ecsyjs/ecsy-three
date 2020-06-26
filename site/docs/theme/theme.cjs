b86ac005f00bee2034bc4359f5782d4cf66fa337
Robert Long <robert@robertlong.me>
2020-06-26 11:50:43 -0700

    Remove typedoc theme

--- site/docs/theme/theme.cjs
+++
@@ -1,9 +0,0 @@
-const Handlebars = require("handlebars");
-
-const MarkdownTheme = require("typedoc-plugin-markdown/dist/theme").default;
-class CustomMarkdownTheme extends MarkdownTheme {
-  constructor(renderer, basePath) {
-    super(renderer, basePath);
-  }
-}
-exports.default = CustomMarkdownTheme;
