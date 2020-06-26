1e0584329a151c2fb486d0a52d20e27200f2bb36
Robert Long <robert@robertlong.me>
2020-06-26 12:01:26 -0700

    Fix Object3DInflator type export

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-export { ECSYThreeWorld } from "./core/world";
+export { ECSYThreeWorld, Object3DInflator } from "./core/world";
 export * from "./core/Types";
 export * from "./core/components";
 export { defaultObject3DInflator } from "./core/defaultObject3DInflator";
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // CORE
 // export { initialize as initializeCore } from "./src/core/initialize.js";
-export { ECSYThreeWorld, Object3DInflator } from "./core/world.js";
+export { ECSYThreeWorld } from "./core/world.js";
 export * from "./core/Types.js";
 export * from "./core/components.js";
 export { defaultObject3DInflator } from "./core/defaultObject3DInflator.js";
