8acf089fd3c48f00b28e1df6fbdfee628e080bf3
Robert Long <robert@robertlong.me>
2020-06-29 09:23:21 -0700

    addObject3DComponent, removeObject3DComponent, remove optional arguments

--- src/core/entity.d.ts
+++ src/core/entity.d.ts
@@ -6,8 +6,8 @@ export interface ECSYThreeObject3D {
 }
 
 export class ECSYThreeEntity extends _Entity {
-  addObject3DComponent(obj: Object3D, parentEntity: Entity): this
-  removeObject3DComponent(unparent: boolean): void
-  remove(forceImmediate: boolean): void
+  addObject3DComponent(obj: Object3D, parentEntity?: Entity): this
+  removeObject3DComponent(unparent?: boolean): void
+  remove(forceImmediate?: boolean): void
   getObject3D<T extends Object3D>(): T & ECSYThreeObject3D
 }
