022e8b794c75a0055ceb0f68083b3028f0548171
Robert Long <robert@robertlong.me>
2020-06-29 09:19:43 -0700

    Export ECSYThreeObject3D and ECSYThreeEntity types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
 export { ECSYThreeWorld, Object3DInflator } from "./core/world";
 export * from "./core/Types";
 export * from "./core/components";
+export * from "./core/entity";
 export { defaultObject3DInflator } from "./core/defaultObject3DInflator";
 export { ECSYThreeSystem } from "./core/system";
