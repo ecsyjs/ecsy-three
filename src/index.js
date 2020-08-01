089ae2002f85cfe1b959131f39707fb26d8bd5c8
Robert Long <robert@robertlong.me>
2020-07-31 23:56:26 -0700

    Fix exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 export { ECSYThreeWorld } from "./world.js";
 export { ECSYThreeSystem } from "./system.js";
 export { defaultObject3DInflator } from "./defaultObject3DInflator.js";
-export * as ThreeTypes from "./types/index.js";
+import * as _ThreeTypes from "./types/index.js";
+export const ThreeTypes = _ThreeTypes;
 export * from "./components/index.js";
 export * from "./systems/index.js";
 export { initialize } from "./initialize.js";
