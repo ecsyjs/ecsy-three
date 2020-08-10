92d46592925862ffe4578c8ed994862b80b3d113
Samuel <sam6321@users.noreply.github.com>
2020-08-10 08:38:02 +0800

    Add missing export from entity.js
    Add missing export from 'entity.js' so index.js matches index.d.ts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { ECSYThreeWorld } from "./world.js";
 export { ECSYThreeSystem } from "./system.js";
+export * from "./entity.js";
 import * as _ThreeTypes from "./types/index.js";
 export const ThreeTypes = _ThreeTypes;
 export * from "./components/index.js";
